281c0645211da5488b0a38edd121e7317954ffef
Kevin Febrian <pownyfals@gmail.com>
2022-03-26 16:12:01 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul5_1302204081/SayaTubeUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204081
+{
+	public class SayaTubeUser
+	{
+		private int id;
+		private LinkedList<SayaTubeVideo> uploadedVideo;
+		private String username;
+		
+
+		public SayaTubeUser(string user)
+		{
+			this.uploadedVideo = new LinkedList<SayaTubeVideo>();
+			this.username = user;
+
+			//mengenerate id number 5 digit
+			Random generator = new Random();
+			int randNum = generator.Next(100000);
+			string fiveDigitsNumber = randNum.ToString("D5");
+			this.id = Int32.Parse(fiveDigitsNumber);
+		}
+		public int getTotalVideoPlayCount()
+		{
+			int total = 0;
+			for(int i = 0; i < uploadedVideo.Count; i++)
+			{
+				total = total + uploadedVideo.ElementAt(i).getPlayCount();	
+			}
+			return total;
+		}
+
+		public void AddVideo(SayaTubeVideo video)
+		{
+			this.uploadedVideo.AddFirst(video);
+		}
+
+		public void printAllVideoPlayCount()
+		{
+			Console.WriteLine("User : "+this.username);
+			for(int i = 0; i < this.uploadedVideo.Count; i++)
+			{
+				Console.WriteLine("Video " + i + " " + this.uploadedVideo.ElementAt(i).getTitle());
+			}
+		}
+	}
+}
---
+++ modul5_1302204081/SayaTubeVideo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204081
+{
+	public class SayaTubeVideo
+	{
+		private int id;
+		private string title;
+		private int playCount;
+		
+
+		public SayaTubeVideo(String title)
+		{
+			int totalStrJudul = title.Length;
+				if (totalStrJudul <= 100)
+				{
+					this.title = title;
+			}
+			else
+			{
+				this.title = " ";
+			}
+			
+
+			//mengenerate id number 5 digit
+			Random generator = new Random();
+			int randNum = generator.Next(100000);
+			string fiveDigitsNumber = randNum.ToString("D5");
+			this.id = Int32.Parse(fiveDigitsNumber);
+			//mengisi playcount = 0
+			this.playCount = 0;
+
+		}
+
+		public void increasePlayCount(int playCount)
+		{
+			this.playCount = playCount;
+
+		}
+		public int getPlayCount()
+		{
+			return this.playCount;
+		}
+		public string getTitle()
+		{
+			return this.title;
+		}
+		public void PrintVideoDetails()
+		{
+			Console.WriteLine("Judul Video = " + this.title);
+			
+		}
+	}
+}
